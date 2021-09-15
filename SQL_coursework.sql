193b74731efcc03e73c5ab91f496869c6eba757d
= <=>
2021-09-15 14:51:24 +0200

    procedure recommend_film added

--- SQL_coursework.sql
+++ SQL_coursework.sql
@@ -347,8 +347,45 @@ FROM
 	films
 JOIN 
 	genres ON genres.id = films.from_genre_id 
-
-	
-	
 	
 
+####################################################################################################
+-- Процедура 
+####################################################################################################	
+
+-- Создать функцию, которая рекомендует случайный фильм из того же жанра
+	
+DROP PROCEDURE IF EXISTS recommend_film;
+	
+DELIMITER // 
+	
+CREATE PROCEDURE recommend_film(in targer_genre_id INT)
+
+BEGIN 
+		
+	SELECT 
+		f2.film_name,
+		g.genre		
+	FROM 
+		films f1
+	JOIN 
+		films f2 ON f1.id = f2.id
+	JOIN 
+		genres g ON g.id = f2.from_genre_id 
+	WHERE 
+		f2.from_genre_id = targer_genre_id		
+	ORDER BY 
+		rand()
+	LIMIT 1;
+	
+END //	
+
+DELIMITER ;
+
+-- запустим процедуру (при каждом запуске будет новый результат)
+CALL recommend_film(1)
+
+
+
+	
+		
